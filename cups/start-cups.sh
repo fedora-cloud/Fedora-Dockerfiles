a52df489a53cb87c7d16a11c4eb54e772d363641
Tim Waugh <twaugh@redhat.com>
2014-10-21 17:50:08 +0100

    cups: Don't be too verbose in start-cups.sh.

--- cups/start-cups.sh
+++ cups/start-cups.sh
@@ -4,7 +4,6 @@ if ! grep -q "^$CUPS_ADMIN_USER:" /etc/shadow; then
 	useradd "$CUPS_ADMIN_USER" --system -G root,sys -M
 	printf "%s\n%s\n" "$CUPS_ADMIN_PASSWORD" "$CUPS_ADMIN_PASSWORD" | \
 		(passwd --stdin "$CUPS_ADMIN_USER")
-	echo Done
 fi
 
 exec /usr/sbin/cupsd -f
