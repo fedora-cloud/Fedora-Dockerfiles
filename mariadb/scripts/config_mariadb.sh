099bdb5a3833c3ce6b370a236f0bf9ed4ec792b2
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2016-03-22 14:31:36 +0000

    mariadb: fix hard-coded 20 second sleep waiting for readiness
    Poll for readiness, like everyone else does.
    
    This bloats the code back up & it got rather harder to read.  So I added
    some comments, in the form of progress and error messages.

--- mariadb/scripts/config_mariadb.sh
+++ mariadb/scripts/config_mariadb.sh
@@ -2,18 +2,43 @@
 
 set -e
 
+echo "Initializing mariadb"
 mysql_install_db --user=mysql
 
+echo "Starting mysqld for initial configuration"
 /usr/bin/mysqld_safe -u mysql &
-sleep 10
+PID=$!
+
+for i in {30..0}; do
+  if (! kill -0 $PID) ||
+     (echo 'SELECT 1' | mysql -u root >/dev/null 2>&1)
+  then
+    break
+  fi
+  sleep 1
+done
+
+if ! kill -0 $PID; then
+  echo
+  echo "mysqld failed to start"
+  exit 1
+fi
+
+if ! (echo 'SELECT 1' | mysql -u root >/dev/null 2>&1); then
+  echo
+  echo "Timeout waiting for mysqld to become ready"
+  exit 1
+fi
 
 USER="${USER-dbuser}"
 PASS="${PASS-$(pwgen -s -1 12)}"
 NAME="${NAME-db}"
+printf "\n"
 printf "root password=\n"
 printf "NAME=%s\n" "$NAME"
 printf "USER=%s\n" "$USER"
 printf "PASS=%s\n" "$PASS"
+printf "\n"
 
 mysql -uroot <<-EOF
 CREATE DATABASE $NAME;
@@ -21,6 +46,6 @@ CREATE USER '$USER'@'%' IDENTIFIED BY '$PASS';
 GRANT ALL PRIVILEGES ON $NAME.* TO '$USER'@'%' WITH GRANT OPTION;
 EOF
 
-killall mysqld
-sleep 10
-
+# mysqld_safe does not support `kill $PID`.
+# Instead, we must copy what the sysV-style init script does.
+mysqladmin shutdown
