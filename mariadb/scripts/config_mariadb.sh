85c64bcd91b83d75d4bff9240e5fb3630544706c
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2016-03-22 10:28:15 +0000

    mariadb: simplify: what functions?
    Functions were not re-used.  They didn't have meaningful names either.
    
    They did print a heading when we'd finished waiting for mysqld to start.
    However the following commit will fix the 10 seconds where we wait and
    do nothing, so even that won't be useful anymore.

--- mariadb/scripts/config_mariadb.sh
+++ mariadb/scripts/config_mariadb.sh
@@ -2,33 +2,25 @@
 
 set -e
 
-__mysql_config() {
-  echo "Running the mysql_config function."
-  mysql_install_db --user=mysql
-  /usr/bin/mysqld_safe -u mysql &
-  sleep 10
-}
+mysql_install_db --user=mysql
 
-__start_mysql() {
-  printf "Running the start_mysql function.\n"
-  USER="${USER-dbuser}"
-  PASS="${PASS-$(pwgen -s -1 12)}"
-  NAME="${NAME-db}"
-  printf "root password=\n"
-  printf "NAME=%s\n" "$NAME"
-  printf "USER=%s\n" "$USER"
-  printf "PASS=%s\n" "$PASS"
-  mysql -uroot <<-EOF
-	CREATE DATABASE $NAME;
-	CREATE USER '$USER'@'%' IDENTIFIED BY '$PASS';
-	GRANT ALL PRIVILEGES ON $NAME.* TO '$USER'@'%' WITH GRANT OPTION;
+/usr/bin/mysqld_safe -u mysql &
+sleep 10
+
+USER="${USER-dbuser}"
+PASS="${PASS-$(pwgen -s -1 12)}"
+NAME="${NAME-db}"
+printf "root password=\n"
+printf "NAME=%s\n" "$NAME"
+printf "USER=%s\n" "$USER"
+printf "PASS=%s\n" "$PASS"
+
+mysql -uroot <<-EOF
+CREATE DATABASE $NAME;
+CREATE USER '$USER'@'%' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON $NAME.* TO '$USER'@'%' WITH GRANT OPTION;
 EOF
 
-  killall mysqld
-  sleep 10
-}
-
-# Call all functions
-__mysql_config
-__start_mysql
+killall mysqld
+sleep 10
 
