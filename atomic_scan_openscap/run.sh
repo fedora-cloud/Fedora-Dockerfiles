6c14c35d71fc9f6ddbf670143d0ba39abba682ac
Martin Preisler <martin@preisler.me>
2016-05-27 12:36:16 -0400

    Typo fix in error message in atomic_scan_openscap/run.sh

--- atomic_scan_openscap/run.sh
+++ atomic_scan_openscap/run.sh
@@ -5,7 +5,7 @@ if [ ! -e /host/etc/atomic.d/atomic_scan_openscap ]; then
     echo "No atomic_scan_openscap file found in /etc/atomic.d.  This image requires you install it with 'atomic install docker.io/atomic_scan_openscap'"
 
 else
-    echo "This container/image is not meant to be run outside of the atomic command. You can use this image by issuing 'atomic scan <container|image  to scanb'.  See 'atomic scan --help' for more information."
+    echo "This container/image is not meant to be run outside of the atomic command. You can use this image by issuing 'atomic scan <container|image>' to scan.  See 'atomic scan --help' for more information."
 
 fi
 
